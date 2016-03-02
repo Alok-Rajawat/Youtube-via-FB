4c34021cd525218959d614960919b7d275ef1dbe
Abdul Fatir <Abdulfatirs@gmail.com>
2016-03-03 00:00:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,126 @@
+import re
+import time
+from selenium import webdriver
+
+def loginToFB(FBdriver, email, password):
+	email_field = FBdriver.find_element_by_id('email')
+	pass_field = FBdriver.find_element_by_id('pass')
+	email_field.send_keys(email)
+	pass_field.send_keys(password)
+	email_field.submit()
+
+def moveToMessages(FBdriver):
+	profile_url = [a for a in FBdriver.find_elements_by_tag_name('a') if a.get_attribute('title') == 'Profile'][0].get_attribute('href')
+	re_search = re.search(r'(\?id=\d+)$', profile_url)
+	
+	FB_ID = ''
+	if re_search:
+		# Profiles with no username
+		FB_ID = re_search.group(0)
+		FB_ID = FB_ID.replace('?id=', '')
+	else:
+		# Profiles with username
+		FB_ID = profile_url[profile_url.rfind('/')+1:]
+	print FB_ID
+
+	FBdriver.get('https://www.facebook.com/messages/'+FB_ID)
+
+	global reply_button
+	reply_button = [x for x in FBdriver.find_elements_by_tag_name('input') if x.get_attribute('value') == 'Reply'][0]
+
+	if not(reply_button.is_displayed()):
+		FBdriver.find_element_by_css_selector('._1s0').click()
+
+def parseMessageAndExecute(message, FBdriver, YoutubeDriver):
+	split_message = message.split()
+	if re.search(r'^(play\s)', message):
+		search_term = ' '.join(split_message[1:])
+		search_box = YoutubeDriver.find_element_by_name('search_query')
+		search_box.send_keys(search_term)
+		search_box.submit()
+		results = YoutubeDriver.find_elements_by_class_name('item-section')[0].find_elements_by_class_name('yt-lockup-title')
+
+		number_of_results = 5
+
+		title_link_map = {}
+		for result in results[:number_of_results]:
+			anchor_tag = result.find_elements_by_tag_name('a')[0]
+			title_link_map[anchor_tag.text.encode('ascii','ignore')] = anchor_tag.get_attribute('href').encode('ascii','ignore')
+		search_result_output = ''
+		k=1
+		num_key_map = []
+		for title in title_link_map.keys():
+			search_result_output = (str(k)+'. '+title+'\n\n')
+			num_key_map.append(title)
+			FBdriver.find_element_by_css_selector('.uiTextareaNoResize.uiTextareaAutogrow._1rv').send_keys(search_result_output)
+			reply_button.click()
+			k+=1
+
+		FBdriver.implicitly_wait(10)
+		time.sleep(1)
+
+		previous_message_state = FBdriver.find_elements_by_class_name('null')
+		while True:
+			FBdriver.implicitly_wait(5)
+			current_message_state = FBdriver.find_elements_by_class_name('null')
+			if not current_message_state == previous_message_state:
+				latest_message = current_message_state[-1].text.encode('ascii','ignore')
+				link = title_link_map[num_key_map[int(latest_message)-1]]
+				YoutubeDriver.get(link)
+				break
+			time.sleep(0.1)
+	elif re.search(r'^(pause)$', message.strip()):
+		YoutubeDriver.execute_script("document.getElementById('movie_player').pauseVideo();")
+	elif re.search(r'^(unpause)$', message.strip()):
+		YoutubeDriver.execute_script("document.getElementById('movie_player').playVideo();")
+	elif re.search(r'^(mute)$', message.strip()):
+		YoutubeDriver.execute_script("document.getElementById('movie_player').mute();")
+	elif re.search(r'^(unmute)$', message.strip()):
+		YoutubeDriver.execute_script("document.getElementById('movie_player').unMute();")
+	elif re.search(r'^(seek-to)\s', message.strip()):
+		time_value = split_message[1]
+		YoutubeDriver.execute_script("document.getElementById('movie_player').seekTo("+time_value+",true);")
+	elif re.search(r'^(play-now)\s', message.strip()):
+		search_term = ' '.join(split_message[1:])
+		search_box = YoutubeDriver.find_element_by_name('search_query')
+		search_box.send_keys(search_term)
+		search_box.submit()
+		link = YoutubeDriver.find_elements_by_class_name('item-section')[0].find_elements_by_class_name('yt-lockup-title')[0].find_elements_by_tag_name('a')[0].get_attribute('href')
+		YoutubeDriver.get(link)
+
+def displayUsage():
+	print '\n\n\t\t=== SETUP COMPLETE ===\n'
+	print '\tUsage:\n'
+	print '\t\tplay <search term> : Returns top 5 videos from YouTube, waits for an integer response to play video'
+	print '\t\tplay-now <search term> : Plays first video from search results (I\'m feeling lucky)'
+	print '\t\tseek-to <time in seconds> : Seeks to the said time'
+	print '\t\tpause : Pauses the video'
+	print '\t\tunpause : Unpauses the video'
+	print '\t\tmute : Mutes the video'
+	print '\t\tunmute : Unmutes the video\n\n'
+
+def readMessages(FBdriver, YoutubeDriver):
+	previous_message_state = FBdriver.find_elements_by_class_name('null')
+	while True:
+		FBdriver.implicitly_wait(5)
+		current_message_state = FBdriver.find_elements_by_class_name('null')
+		if not current_message_state == previous_message_state:
+			latest_message = current_message_state[-1].text
+			print latest_message
+			parseMessageAndExecute(latest_message, FBdriver, YoutubeDriver)
+			previous_message_state = current_message_state
+		time.sleep(0.1)
+
+if __name__ == '__main__':
+	email = 'abdulfatir@outlook.com'
+	password = 'Seriously, bro?'
+
+	FBdriver = webdriver.Chrome()
+	YoutubeDriver = webdriver.Chrome()
+	FBdriver.get('https://www.facebook.com')
+	YoutubeDriver.get('https://www.youtube.com')
+
+	loginToFB(FBdriver, email, password)
+	moveToMessages(FBdriver)
+	displayUsage()
+	readMessages(FBdriver, YoutubeDriver)
